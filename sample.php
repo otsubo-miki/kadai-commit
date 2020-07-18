ca157b28b859aaae14e8957a02880656430ca574
akira.takizawa <akira_takizawa@mirai.re>
2020-07-18 07:41:39 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,7 @@
+<?php
+$result = "無し";
+?>
+
 <!DOCTYPE html>
 <html lang = "ja">
     <head>
@@ -6,7 +10,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p1>おみくじの結果：無し</p1>
+        <p1>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES,"UTF-8");?></p1>
         <form action="sample.php" method="post">
             <buttn type="submit" name="act" value = "draw">おみくじをひく！</buttn>
         </form>
