2318ef99ae511fef997b56885720ed7e83cb0442
akira.takizawa <akira_takizawa@mirai.re>
2020-07-18 07:35:21 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "ja">
+    <head>
+        <meta charset = "UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p1>おみくじの結果：無し</p1>
+        <form action="sample.php" method="post">
+            <buttn type="submit" name="act" value = "draw">おみくじをひく！</buttn>
+        </form>
+    </body>
+</html>
+    
